9a01a3a39d3c65218aa38894a4de681b7378a020
Kyle Barron <kylebarron2@gmail.com>
2018-03-28 20:40:55 -0400

    Import codes.npi class into global package namespace

--- medicare_utils/__init__.py
+++ medicare_utils/__init__.py
@@ -6,6 +6,6 @@ __author__ = """Kyle Barron"""
 __email__ = 'barronk@mit.edu'
 __version__ = '0.0.1'
 
-from .codes import icd9, hcpcs
+from .codes import icd9, hcpcs, npi
 from .utils import fpath, MedicareDF, pq_vars
 from . import parquet
