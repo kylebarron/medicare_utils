ca2da07ec013d3da8957e02254ea547591a23397
Kyle Barron <barronk@mit.edu>
2018-07-09 12:35:50 -0400

    Use Sphinx bootstrap theme - https://github.com/ryan-roemer/sphinx-bootstrap-theme

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
+import sphinx_bootstrap_theme
 import medicare_utils
 
 # -- General configuration ---------------------------------------------
@@ -98,7 +99,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
