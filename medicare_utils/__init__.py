3f34c4863567d2a525be225acd6f844f2aed9d75
Kyle Barron <barronk@mit.edu>
2019-03-29 15:33:18 -0400

    Update version to 0.1.0

--- medicare_utils/__init__.py
+++ medicare_utils/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Kyle Barron"""
 __email__ = 'barronk@mit.edu'
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 
 from .codes import icd9, hcpcs, npi
 from .utils import fpath, pq_vars
--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/kylebarron/medicare_utils',
-    version='0.0.2',
+    version='0.1.0',
     zip_safe=False,
 )
